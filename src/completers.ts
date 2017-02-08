5465406dca1575884ff31671e7a59ac5f5b06943
Clemens Prerovsky <c.prerovsky@gentics.com>
2017-02-08 15:58:01 +0100

    refactored completers

--- src/completers.ts
+++ src/completers.ts
@@ -1,16 +1,30 @@
 import { State } from './index';
-import { MeshAPI } from 'mesh-api-client';
+import { MeshAPI, ProjectsProjectUuidGetResponse } from 'mesh-api-client';
 import defaultCompleter from './completers/default';
 import nodeChildrenCompleter from './completers/nodechildren';
 
 type Completer = (mesh: MeshAPI, line: string, cmd: string[], state: State) => Promise<[string[], string]>;
+
 interface CompleterTable { [key: string]: Completer }
+
+let defaultNodeChildrenQuery = (state, mesh) => mesh.api.project(state.project).nodes.nodeUuid(state.current.uuid).children;
+let uuidReducer = (prev, node) => prev.concat(node.uuid);
+
 export const COMPLETERS: CompleterTable = {
     defaultCompleter: defaultCompleter,
-    cd: nodeChildrenCompleter((node, cmd) => {
-        return node.container && (node.uuid.indexOf(cmd[1]) === 0 || node.fields.name.indexOf(cmd[1]) === 0);
-    }),
-    read: nodeChildrenCompleter((node, cmd) => {
-        return node.uuid.indexOf(cmd[1]) === 0;
-    })
+    cd: nodeChildrenCompleter(
+        defaultNodeChildrenQuery,
+        (node, cmd) => node.container && (node.uuid.indexOf(cmd[1]) === 0 || node.fields.name.indexOf(cmd[1]) === 0),
+        uuidReducer
+    ),
+    project: nodeChildrenCompleter<ProjectsProjectUuidGetResponse>(
+        (state, mesh) => mesh.api.projects,
+        (node, cmd) => node.name.indexOf(cmd[1]) === 0,
+        (prev, node) => prev.concat(node.name)
+    ),
+    read: nodeChildrenCompleter(
+        defaultNodeChildrenQuery,
+        (node, cmd) => node.uuid.indexOf(cmd[1]) === 0,
+        uuidReducer
+    )
 }
--- src/completers/nodechildren.ts
+++ src/completers/nodechildren.ts
@@ -2,10 +2,19 @@ import { MeshAPI, ProjectNodesNodeUuidGetResponse } from 'mesh-api-client';
 import { State } from '../index';
 import * as readline from 'readline';
 
-export default function nodeChildrenCompleter(resultFilter: (node: ProjectNodesNodeUuidGetResponse, cmd: string[]) => boolean) {
+export interface MeshQuery {
+    (state: State, mesh: MeshAPI): { get: () => Promise<any> }
+}
+
+export default function nodeChildrenCompleter<T>(
+    query: MeshQuery,
+    filter: (node: T, cmd: string[]) => boolean,
+    reducer: (prev: string[], node: T) => string[]
+) {
     return async (mesh: MeshAPI, line: string, cmd: string[], state: State): Promise<readline.CompleterResult> => {
         return new Promise<readline.CompleterResult>(async (resolve, reject) => {
-            let nodes = await mesh.api.project(state.project).nodes.nodeUuid(state.current.uuid).children.get();
+            // let nodes = await mesh.api.project(state.project).nodes.nodeUuid(state.current.uuid).children.get();
+            let nodes = await query(state, mesh).get();
             if (cmd.length === 1) {
                 resolve([
                     nodes.data.reduce((prev, curr) => {
@@ -15,10 +24,8 @@ export default function nodeChildrenCompleter(resultFilter: (node: ProjectNodesN
                 ]);
             }
             let found: string[] = nodes.data.filter((node) => {
-                return resultFilter(node, cmd);
-            }).reduce((prev, node) => {
-                return prev.concat(node.uuid);
-            }, []);
+                return filter(node, cmd);
+            }).reduce(reducer, []);
             resolve([found, cmd[1]]);
         });
     }
