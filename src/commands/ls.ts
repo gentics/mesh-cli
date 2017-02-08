419a1bce0d30ea11279acde099905d6feeb95631
Clemens Prerovsky <c.prerovsky@gentics.com>
2017-02-08 15:05:32 +0100

    made read automatically read current node when no uuid is given

--- src/commands/ls.ts
+++ src/commands/ls.ts
@@ -4,7 +4,7 @@ export default async function ls(mesh: MeshAPI, cmd: string[], state: State): Pr
     return new Promise<State>(async (resolve, reject) => {
         let nodes = await mesh.api.project(state.project).nodes.nodeUuid(state.current.uuid).children.get();
         console.log(nodes.data.reduce((out, node) => {
-            let type = node.container ? 'DIR' : '   ';
+            let type = node.container ? 'DIR ' : 'NODE';
             return `${out}\n${type} ${node.uuid} ${node.fields.name}`;
         }, ''));
         resolve(state);
--- src/commands/read.ts
+++ src/commands/read.ts
@@ -2,8 +2,15 @@ import { MeshAPI } from 'mesh-api-client';
 import { State } from '../index';
 export default async function read(mesh: MeshAPI, cmd: string[], state: State): Promise<State> {
     return new Promise<State>(async (resolve, reject) => {
-        let node = await mesh.api.project(state.project).nodes.nodeUuid(cmd[1]).get();
-        console.log(JSON.stringify(node, null, 4));
-        resolve(state);
+        let uuid = cmd.length === 1 ? state.current.uuid : cmd[1];
+        mesh.api.project(state.project).nodes.nodeUuid(uuid).get()
+            .then((node) => {
+                console.log(JSON.stringify(node, null, 4));
+                resolve(state);
+            })
+            .catch((reason) => {
+                console.error(reason);
+                resolve(state);
+            });
     });
 }
