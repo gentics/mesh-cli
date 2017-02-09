c0df3aec4a7337d2177812e4fdc4fc049e19e4fc
Clemens Prerovsky <c.prerovsky@gentics.com>
2017-02-09 10:32:56 +0100

    converted indentation to 4 spaces

--- src/commands/project.ts
+++ src/commands/project.ts
@@ -2,19 +2,19 @@ import { MeshAPI } from 'mesh-api-client';
 import { State } from '../mesh-cli';
 
 export default async function project(mesh: MeshAPI, line: string, cmd: string[], state: State): Promise<State> {
-  return new Promise<State>(async (resolve, reject) => {
-    let projects = await mesh.api.projects.get();
-    let p = projects.data.filter((p) => p.name === cmd[1]);
-    if (p.length === 0) {
-      reject('No such project.');
-    } else {
-      mesh.api.project(p[0].name).nodes.nodeUuid(p[0].rootNodeUuid).get({ version: 'draft' })
-        .then((node) => {
-          resolve(new State(state.rl, cmd[1], node));
-        })
-        .catch((e) => {
-          reject(e);
-        });
-    }
-  });
+    return new Promise<State>(async (resolve, reject) => {
+        let projects = await mesh.api.projects.get();
+        let p = projects.data.filter((p) => p.name === cmd[1]);
+        if (p.length === 0) {
+            reject('No such project.');
+        } else {
+            mesh.api.project(p[0].name).nodes.nodeUuid(p[0].rootNodeUuid).get({ version: 'draft' })
+            .then((node) => {
+                resolve(new State(state.rl, cmd[1], node));
+            })
+            .catch((e) => {
+                reject(e);
+            });
+        }
+    });
 }
--- src/mesh-cli.ts
+++ src/mesh-cli.ts
@@ -7,7 +7,7 @@ import { COMMANDS } from './commands';
 import { COMPLETERS } from './completers';
 
 export class State {
-  constructor(readonly rl: readline.ReadLine, readonly project: string, readonly current: ProjectNodesNodeUuidGetResponse) { }
+    constructor(readonly rl: readline.ReadLine, readonly project: string, readonly current: ProjectNodesNodeUuidGetResponse) { }
 }
 
 let mesh = new MeshAPI({ debug: false });
@@ -15,48 +15,48 @@ let state = new State(null, '', null);
 let rl;
 
 mesh.api.auth.login.post({ username: 'admin', password: 'admin' }).then(() => {
-  rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    completer: completer
-  });
-  rl.on('line', onLine);
-  state = new State(rl, '', null);
-  onLine('project demo');
+    rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        completer: completer
+    });
+    rl.on('line', onLine);
+    state = new State(rl, '', null);
+    onLine('project demo');
 });
 
 function completer(line: string, callback?: (err: any, result: readline.CompleterResult) => void): any {
-  let cmd = line.split(' ');
-  if (COMPLETERS[cmd[0]]) {
-    COMPLETERS[cmd[0]](mesh, line, cmd, state)
-      .then((result) => {
-        callback(null, result);
-      }).catch((err) => {
-        callback(err, [[], line]);
-      });
-  } else {
-    COMPLETERS['defaultCompleter'](mesh, line, cmd, state).then((result) => {
-      callback(null, result);
-    }).catch((err) => {
-      callback(err, [[], line]);
-    });
-  }
+    let cmd = line.split(' ');
+    if (COMPLETERS[cmd[0]]) {
+        COMPLETERS[cmd[0]](mesh, line, cmd, state)
+        .then((result) => {
+            callback(null, result);
+        }).catch((err) => {
+            callback(err, [[], line]);
+        });
+    } else {
+        COMPLETERS['defaultCompleter'](mesh, line, cmd, state).then((result) => {
+            callback(null, result);
+        }).catch((err) => {
+            callback(err, [[], line]);
+        });
+    }
 }
 
 async function onLine(line: string) {
-  let cmd = line.split(' ');
-  if (COMMANDS[cmd[0]]) {
-    COMMANDS[cmd[0]](mesh, line, cmd, state)
-      .then((newState) => {
-        state = newState;
-        state.rl.setPrompt(`${state.project}:${state.current.uuid}$ `);
+    let cmd = line.split(' ');
+    if (COMMANDS[cmd[0]]) {
+        COMMANDS[cmd[0]](mesh, line, cmd, state)
+        .then((newState) => {
+            state = newState;
+            state.rl.setPrompt(`${state.project}:${state.current.uuid}$ `);
+            state.rl.prompt();
+        }).catch((e) => {
+            console.error('ERROR', e);
+            state.rl.prompt();
+        });
+    } else {
+        console.error('Unknown command ' + cmd[0]);
         state.rl.prompt();
-      }).catch((e) => {
-        console.error('ERROR', e);
-        state.rl.prompt();
-      });
-  } else {
-    console.error('Unknown command ' + cmd[0]);
-    state.rl.prompt();
-  }
+    }
 }
