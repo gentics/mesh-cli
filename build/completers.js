621444410a0105a306b6a182cdeb4b5008c4efaa
Clemens Prerovsky <c.prerovsky@gentics.com>
2017-02-17 14:34:39 +0100

    compiled, tested & bumped version

--- build/completers.js
+++ build/completers.js
@@ -5,7 +5,7 @@ let defaultNodeChildrenQuery = (state, mesh) => mesh.api.project(state.project).
 let uuidReducer = (prev, node) => prev.concat(node.uuid);
 exports.COMPLETERS = {
     defaultCompleter: default_1.default,
-    cd: nodechildren_1.default(defaultNodeChildrenQuery, (node, cmd) => node.container && (node.uuid.indexOf(cmd[1]) === 0 || node.fields.name.indexOf(cmd[1]) === 0), uuidReducer),
+    cd: nodechildren_1.default(defaultNodeChildrenQuery, (node, cmd) => node.container && node.uuid.indexOf(cmd[1]) === 0, uuidReducer),
     delete: nodechildren_1.default(defaultNodeChildrenQuery, (node, cmd) => node.uuid.indexOf(cmd[1]) === 0, uuidReducer),
     project: nodechildren_1.default((state, mesh) => mesh.api.projects, (node, cmd) => node.name.indexOf(cmd[1]) === 0, (prev, node) => prev.concat(node.name)),
     read: nodechildren_1.default(defaultNodeChildrenQuery, (node, cmd) => node.uuid.indexOf(cmd[1]) === 0, uuidReducer),
